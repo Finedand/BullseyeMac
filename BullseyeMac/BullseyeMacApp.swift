2fd89e0d645676cf9b0fc7cb4a1ca3b5d3d45b47
Aleksandar Veljic <Aveljic@icloud.com>
2021-01-22 22:56:24 +0100

    Initial Commit

---
+++ BullseyeMac/BullseyeMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeMacApp.swift
+//  BullseyeMac
+//
+//  Created by Aleksandar Veljic on 22.01.21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
